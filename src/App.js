86be2bd65483d10e0430de44f9c65e1150196c0e
minhajspace <minhaj.space@gmail.com>
2019-12-24 13:36:56 +0530

    using text field

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from "react";
-import UserInput from "./userInput";
-import Default from "./Defalut";
-
 import "./App.css";
+const orignal = ["wint", "is", "coming"];
 
 class App extends React.Component {
+  state = {
+    data: ["is ", "coming", "winter"],
+    userInput: []
+  };
+
+  onClick(event) {}
   render() {
     return (
       <>
-        <UserInput />
-        <Default />
+        <div className="field">
+          <h3>Pick the Word </h3>
+          <h4>winter is coming</h4>
+          <div className="ui card">
+            <h4>hello</h4>
+          </div>
+
+          <div className="ui input">
+            <form>
+              <input type="text" value={this.state.data[0]}></input>
+              <input type="text" value={this.state.data[1]}></input>
+              <input type="text" value={this.state.data[2]}></input>
+            </form>
+          </div>
+        </div>
       </>
     );
   }
